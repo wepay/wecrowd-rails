941e7af02b70c19e3ad3775a6cd177d05e1c72db
Andrew LeBlanc <andrew@everribbon.com>
2014-09-10 15:33:16 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,65 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.1.5'
+
+# This needs to be with require false as it is
+# loaded after loading the test library to
+# ensure correct loading order
+gem 'mocha', '~> 0.14', require: false
+
+gem 'rack-cache', '~> 1.2'
+gem 'jquery-rails', '~> 3.1.0'
+#gem 'turbolinks'
+#gem 'coffee-rails', '~> 4.0.0'
+
+# require: false so bcrypt is loaded only when has_secure_password is used.
+# This is to avoid ActiveModel (and by extension the entire framework)
+# being dependent on a binary library.
+gem 'bcrypt', '~> 3.1.7', require: false
+
+# This needs to be with require false to avoid
+# it being automatically loaded by sprockets
+gem 'uglifier', '>= 1.3.0', require: false
+
+group :doc do
+  gem 'sdoc', '~> 0.4.0'
+  gem 'redcarpet', '~> 2.2.2', platforms: :ruby
+  gem 'w3c_validators'
+  gem 'kindlerb'
+end
+
+# AS
+gem 'dalli', '>= 2.2.1'
+
+# Add your own local bundler stuff
+local_gemfile = File.dirname(__FILE__) + "/.Gemfile"
+instance_eval File.read local_gemfile if File.exist? local_gemfile
+
+group :test do
+  # FIX: Our test suite isn't ready to run in random order yet
+  gem 'minitest', '< 5.3.4'
+
+  platforms :mri_19 do
+    gem 'ruby-prof', '~> 0.11.2'
+  end
+
+  # platforms :mri_19, :mri_20 do
+  #   gem 'debugger'
+  # end
+
+  gem 'benchmark-ips'
+end
+
+platforms :ruby do
+  gem 'nokogiri', '>= 1.4.5'
+
+  # Needed for compiling the ActionDispatch::Journey parser
+  gem 'racc', '>=1.4.6', require: false
+
+  # AR
+  gem 'sqlite3', '~> 1.3.6'
+
+  group :db do
+    gem 'mysql2', '>= 0.3.13'
+  end
+end
---
+++ provision.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+# Hide the stdin is not a tty garbage...
+sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo 'Updating APT...'
+apt-get -y update >/dev/null 2>&1
+
+echo 'Installing git, curl, build-essential, libssl-dev, libreadline-dev...'
+apt-get -y install git curl build-essential libssl-dev libreadline-dev >/dev/null 2>&1
+
+# Install rbenv
+echo 'Installing rbenv...'
+sudo -u vagrant git clone git://github.com/sstephenson/rbenv.git /home/vagrant/.rbenv
+sudo -u vagrant echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/vagrant/.profile
+sudo -u vagrant echo 'eval "$(rbenv init -)"' >> /home/vagrant/.profile
+sudo -u vagrant git clone git://github.com/sstephenson/ruby-build.git /home/vagrant/.rbenv/plugins/ruby-build
+
+sudo -u vagrant -i rbenv install 2.0.0-p247
+sudo -u vagrant -i rbenv global 2.0.0-p247
+# Sanity check
+sudo -u vagrant -i ruby -v
+
+# Install mysql
+echo 'Installing mysql...'
+
+apt-get -y install  mysql-client mysql-server libmysqlclient-dev libmysql-ruby >/dev/null 2>&1
+service mysql restart
+
+mysql -u root -e ";CREATE DATABASE wecrowd;"
+
+# Install Nginx
+echo 'Installing nginx...'
+apt-get -y install nginx >/dev/null 2>&1
+sudo cp /vagrant/nginx /etc/nginx/sites-enabled/everbutton
+sudo /etc/init.d/nginx restart
+
+# Update Gems
+echo 'Updating gems...'
+sudo -u vagrant -i gem update --system
+
+## Vars
+# export g_wepay_client_id='';
+# export g_wepay_client_secret='';
+# export g_database_username='';
+# export g_database_name='';
+# export g_database_password='';
+
+
+# Slap 'at pig!
+echo "Install project gems..."
+sudo -u vagrant -i gem install bundler
+sudo -u vagrant -i bundle install --gemfile=/vagrant/Gemfile
+sudo -u vagrant echo 'export PORT=3000' >> /home/vagrant/.profile
+sudo -u vagrant echo 'export RAILS_ENV=development' >> /home/vagrant/.profile
+
+echo "Running migrations..."
+sudo -u vagrant -i rake -f /vagrant/Rakefile db:migrate
+sudo -u vagrant -i rake -f /vagrant/Rakefile db:seed
+#sudo -u vagrant -i rake -f /vagrant/Rakefile db:schema:load
+# sudo -u vagrant -i foreman start -d /vagrant
