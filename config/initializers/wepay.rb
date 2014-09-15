1ec279a598ad42e626d25a29fbc4ebc279a30114
Andrew LeBlanc <andrew@everribbon.com>
2014-09-15 15:10:36 -0700

    Remove debugging lines

--- config/initializers/wepay.rb
+++ config/initializers/wepay.rb
@@ -1,7 +1,2 @@
-
-p ENV['WECROWD_CLIENT_ID']
-
 WEPAY_CONFIG = YAML.load_file("#{Rails.root.to_s}/config/wepay.yml")[Rails.env]
 WEPAY = WePay.new(WEPAY_CONFIG['client_id'], WEPAY_CONFIG['client_secret'], WEPAY_CONFIG['use_stage'], true)
-
-p WEPAY_CONFIG
